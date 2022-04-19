2b7b2a7cac481728393d1ff9a4e16eddb5722a22
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-04-19 07:39:09 +0530

    fix:

--- utils/data.js
+++ utils/data.js
@@ -1,3 +1,31 @@
+export const blogData = [
+	{
+		id: "1",
+		name: "Private Routes in React Router v6",
+		heading:
+			"How to create private routes in React router? How did we use to create it earlier and what all has changed in the latest version of React Router?",
+		image: "/assets/privateRoutes.png",
+		website: "https://blog.utsavkumar.tech/private-routes-in-react-router-v6",
+	},
+	{
+		id: "2",
+		name: "Every Render has its own everything",
+		heading:
+			"In this blog, I would try to make you understand how React really works under the hood and what exactly happens on each render and re-render.",
+		image: "/assets/everyRender.png",
+		website: "https://blog.utsavkumar.tech/every-render-has-its-own-everything",
+	},
+	{
+		id: "3",
+		name: "Introduction to JavaScript Proptypes",
+		heading:
+			" What is a Prototype? Why are classes nothing but syntactic sugar and how does a class implement prototypes under the hood in JavaScript?",
+		image: "/assets/prototypes.png",
+		website:
+			"https://blog.utsavkumar.tech/introduction-to-javascript-prototypes",
+	},
+];
+
 export const projectData = [
 	{
 		id: "1",
